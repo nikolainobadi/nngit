9bf16b859781bd2ef3a1d51a36e2c8875f947c5f
Nikolai Nobadi <nikolai.nobadi@gmail.com>
2025-06-08 15:51:38 -0700

    Update SwitchBranch

--- Sources/nngit/DeleteBranch.swift
+++ Sources/nngit/DeleteBranch.swift
@@ -5,6 +5,8 @@
 //  Created by Nikolai Nobadi on 6/8/25.
 //
 
+import SwiftPicker
+import GitShellKit
 import ArgumentParser
 
 extension Nngit {
@@ -14,7 +16,35 @@ extension Nngit {
         )
         
         func run() throws {
+            let picker = SwiftPicker()
+            let shell = GitShellAdapter()
+            let eligibleBranches = try loadEligibleBranches(shell: shell)
+            let branchesToDelete = picker.multiSelection("Select which branches to delete", items: eligibleBranches)
+        
+            for branch in branchesToDelete {
+                if branch.isMerged {
+                    try deleteBranch(branch)
+                } else {
+                    try picker.requiredPermission("") // TODO: -
+                    try deleteBranch(branch, forced: true)
+                }
+            }
             
+            try pruneOrigin()
         }
+    }
+}
+
+extension Nngit.DeleteBranch {
+    func loadEligibleBranches(shell: GitShell) throws -> [GitBranch] {
+        return [] // TODO: -
+    }
+    
+    func deleteBranch(_ branch: GitBranch, forced: Bool = false) throws {
+        // TODO: -
+    }
+    
+    func pruneOrigin() throws {
+        // TODO: -
     }
 }
--- Sources/nngit/SwitchBranch.swift
+++ Sources/nngit/SwitchBranch.swift
@@ -5,6 +5,7 @@
 //  Created by Nikolai Nobadi on 6/8/25.
 //
 
+import Foundation
 import SwiftPicker
 import GitShellKit
 import ArgumentParser
@@ -37,8 +38,24 @@ extension Nngit {
 extension Nngit.SwitchBranch {
     func loadLocalBranches(shell: GitShell) throws -> [GitBranch] {
         try shell.verifyLocalGitExists()
+        let branchNames = try loadBranchNames(shell: shell)
+        let mergedOutput = try shell.runWithOutput(makeGitCommand(.listMergedBranches, path: nil))
+        let mergedBranches = Set(mergedOutput.split(separator: "\n").map { $0.trimmingCharacters(in: .whitespaces) })
         
-        return []
+        return branchNames.map { name in
+            let isCurrentBranch = name.hasPrefix("*")
+            let cleanBranchName = isCurrentBranch ? String(name.dropFirst(2)) : name
+            let isMerged = cleanBranchName == "main" ? true : mergedBranches.contains(cleanBranchName)
+            var creationDate: Date?
+            
+            if let dateOutput = try? shell.runWithOutput(makeGitCommand(.getBranchCreationDate(cleanBranchName), path: nil)) {
+                creationDate = ISO8601DateFormatter().date(from: dateOutput.trimmingCharacters(in: .whitespacesAndNewlines))
+            }
+            
+            let syncStatus = try? getSyncStatus(branchName: name, shell: shell)
+            
+            return .init(name: cleanBranchName, isMerged: isMerged, isCurrentBranch: isCurrentBranch, creationDate: creationDate, syncStatus: syncStatus ?? .undetermined)
+        }
     }
     
     func loadBranchNames(shell: GitShell) throws -> [String] {
@@ -47,6 +64,33 @@ extension Nngit.SwitchBranch {
         return output
             .split(separator: "\n")
             .map({ $0.trimmingCharacters(in: .whitespaces) })
+    }
+    
+    func getSyncStatus(branchName: String, comparingBranch: String? = nil, shell: GitShell) throws -> BranchSyncStatus {
+        guard try shell.remoteExists(path: nil) else {
+            return .noRemoteBranch
+        }
+        
+        let remoteBranch = "origin/\(comparingBranch ?? branchName)"
+        let comparisonResult = try shell.runWithOutput(makeGitCommand(.compareBranchAndRemote(local: branchName, remote: remoteBranch), path: nil))
+        let changes = comparisonResult.split(separator: "\t").map(String.init)
+        
+        guard changes.count == 2 else {
+            return .undetermined
+        }
+        
+        let ahead = changes[0]
+        let behind = changes[1]
+        
+        if ahead == "0" && behind == "0" {
+            return .nsync
+        } else if ahead != "0" && behind == "0" {
+            return .ahead
+        } else if ahead == "0" && behind != "0" {
+            return .behind
+        } else {
+            return .diverged
+        }
     }
 }
 

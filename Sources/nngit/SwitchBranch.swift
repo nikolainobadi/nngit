9670cc86307227b3eacb70e16f3f9b2019a2f8da
Nikolai Nobadi <nikolai.nobadi@gmail.com>
2025-06-08 15:39:37 -0700

    Add switchBranch feature

--- Sources/nngit/SwitchBranch.swift
+++ Sources/nngit/SwitchBranch.swift
@@ -5,6 +5,8 @@
 //  Created by Nikolai Nobadi on 6/8/25.
 //
 
+import SwiftPicker
+import GitShellKit
 import ArgumentParser
 
 extension Nngit {
@@ -14,7 +16,42 @@ extension Nngit {
         )
         
         func run() throws {
+            let picker = SwiftPicker()
+            let shell = GitShellAdapter()
+            let branchList = try loadLocalBranches(shell: shell)
+            let currentBranch = branchList.first(where: { $0.isCurrentBranch })
+            let otherBranches = branchList.filter({ !$0.isCurrentBranch})
             
+            var details = ""
+            
+            if let currentBranch {
+                details = "(switching from \(currentBranch.name)"
+            }
+            
+            let selectedBranch = try picker.requiredSingleSelection("Select a branch \(details)", items: otherBranches)
+            let _ = try shell.runWithOutput(makeGitCommand(.switchBranch(selectedBranch.name), path: nil))
         }
+    }
+}
+
+extension Nngit.SwitchBranch {
+    func loadLocalBranches(shell: GitShell) throws -> [GitBranch] {
+        try shell.verifyLocalGitExists()
+        
+        return []
+    }
+    
+    func loadBranchNames(shell: GitShell) throws -> [String] {
+        let output = try shell.runWithOutput(makeGitCommand(.listLocalBranches, path: nil))
+        
+        return output
+            .split(separator: "\n")
+            .map({ $0.trimmingCharacters(in: .whitespaces) })
+    }
+}
+
+extension GitBranch: DisplayablePickerItem {
+    var displayName: String {
+        return name
     }
 }

b17a3d37f0b73be3f919efd9175020b31c9f0f0d
Nikolai Nobadi <nikolai.nobadi@gmail.com>
2025-07-04 14:25:45 -0700

    Update GitConfig to include parameters for branch loading

--- Sources/nngit/Git/Config/GitConfig.swift
+++ Sources/nngit/Git/Config/GitConfig.swift
@@ -10,15 +10,24 @@ struct GitConfig: Codable {
     var branchPrefixList: [BranchPrefix]
     var rebaseWhenBranchingFromDefaultBranch: Bool
     var pruneWhenDeletingBranches: Bool
+    var loadMergeStatusWhenLoadingBranches: Bool
+    var loadCreationDateWhenLoadingBranches: Bool
+    var loadSyncStatusWhenLoadingBranches: Bool
 
     init(defaultBranch: String,
          branchPrefixList: [BranchPrefix],
          rebaseWhenBranchingFromDefaultBranch: Bool,
-         pruneWhenDeletingBranches: Bool = false) {
+         pruneWhenDeletingBranches: Bool = false,
+         loadMergeStatusWhenLoadingBranches: Bool = true,
+         loadCreationDateWhenLoadingBranches: Bool = true,
+         loadSyncStatusWhenLoadingBranches: Bool = true) {
         self.defaultBranch = defaultBranch
         self.branchPrefixList = branchPrefixList
         self.rebaseWhenBranchingFromDefaultBranch = rebaseWhenBranchingFromDefaultBranch
         self.pruneWhenDeletingBranches = pruneWhenDeletingBranches
+        self.loadMergeStatusWhenLoadingBranches = loadMergeStatusWhenLoadingBranches
+        self.loadCreationDateWhenLoadingBranches = loadCreationDateWhenLoadingBranches
+        self.loadSyncStatusWhenLoadingBranches = loadSyncStatusWhenLoadingBranches
     }
 
     enum CodingKeys: String, CodingKey {
@@ -26,6 +35,9 @@ struct GitConfig: Codable {
         case branchPrefixList
         case rebaseWhenBranchingFromDefaultBranch
         case pruneWhenDeletingBranches
+        case loadMergeStatusWhenLoadingBranches
+        case loadCreationDateWhenLoadingBranches
+        case loadSyncStatusWhenLoadingBranches
     }
 
     init(from decoder: Decoder) throws {
@@ -34,6 +46,9 @@ struct GitConfig: Codable {
         branchPrefixList = try container.decode([BranchPrefix].self, forKey: .branchPrefixList)
         rebaseWhenBranchingFromDefaultBranch = try container.decode(Bool.self, forKey: .rebaseWhenBranchingFromDefaultBranch)
         pruneWhenDeletingBranches = try container.decodeIfPresent(Bool.self, forKey: .pruneWhenDeletingBranches) ?? false
+        loadMergeStatusWhenLoadingBranches = try container.decodeIfPresent(Bool.self, forKey: .loadMergeStatusWhenLoadingBranches) ?? true
+        loadCreationDateWhenLoadingBranches = try container.decodeIfPresent(Bool.self, forKey: .loadCreationDateWhenLoadingBranches) ?? true
+        loadSyncStatusWhenLoadingBranches = try container.decodeIfPresent(Bool.self, forKey: .loadSyncStatusWhenLoadingBranches) ?? true
     }
 }
 
@@ -42,6 +57,9 @@ extension GitConfig {
         return .init(defaultBranch: "main",
                     branchPrefixList: [],
                     rebaseWhenBranchingFromDefaultBranch: true,
-                    pruneWhenDeletingBranches: false)
+                    pruneWhenDeletingBranches: false,
+                    loadMergeStatusWhenLoadingBranches: true,
+                    loadCreationDateWhenLoadingBranches: true,
+                    loadSyncStatusWhenLoadingBranches: true)
     }
 }

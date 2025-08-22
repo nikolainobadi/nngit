2cd1f088e0eb7e529d6163091806fe16c33a66f2
Nikolai Nobadi <nikolai.nobadi@gmail.com>
2025-08-22 16:22:16 -0700

    WIP -> start new logic in NewBranchManager

--- Sources/nngit/Git/NewBranchManager.swift
+++ Sources/nngit/Git/NewBranchManager.swift
@@ -26,6 +26,38 @@ extension NewBranchManager {
     /// Handles remote repository synchronization when creating a new branch.
     /// Checks if current branch is the default branch and handles sync status.
     func handleRemoteRepository() throws {
+        guard let currentBranch = try loadCurrentBranch() else {
+            return // TODO: -
+        }
         
+        if isCurrentBranch(currentBranch) {
+            let mainBranchStatus = try branchLoader.getSyncStatus(branchName: currentBranch.name, comparingBranch: nil, remoteExists: true)
+            
+            switch mainBranchStatus {
+            case .behind:
+                break
+            case .ahead:
+                break
+            case .nsync:
+                break
+            default:
+                break
+            }
+        } else {
+            // TODO: - check if a remote branch exists
+            // if it does, get status
+        }
+    }
+}
+
+
+// MARK: - Private Methods
+private extension NewBranchManager {
+    func loadCurrentBranch() throws -> GitBranch? {
+        return try branchLoader.loadBranches(for: nil, mainBranchName: config.defaultBranch).first(where: { $0.isCurrentBranch })
+    }
+    
+    func isCurrentBranch(_ branch: GitBranch) -> Bool {
+        return false // TODO: -
     }
 }
